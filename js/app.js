77b27b900c188a52ec75fe6e5483000d3b59b62e
Felipe Komata <fbkomata@gmail.com>
2025-05-07 11:26:08 -0300

    subtração dos ingressos

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 function comprar(){
     let tipoDeCadeira = document.getElementById('tipo-ingresso').value;
     let quantidadeIngresso = document.getElementById('qtd').value;
-   let totalIngressos = document.getElementById(`qtd-${tipoDeCadeira}`).textContent
-
-   console.log(totalIngressos);
-
+    let totalIngressos = document.getElementById(`qtd-${tipoDeCadeira}`).textContent;
     
+    let resultanteCompra = totalIngressos - quantidadeIngresso;
+    
+    if(resultanteCompra <= 0){
+        alert('a compra não pode ser realizada');
+        return 
+    }
+
+    let ingressosFinal = document.getElementById(`qtd-${tipoDeCadeira}`);
+    ingressosFinal.innerHTML = resultanteCompra;
+
 }
 
 
